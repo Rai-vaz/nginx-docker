5df9beb48898e4a41bbb474bdaabaa9f1b7a6d5f
Rai-vaz <ray.vaz04@gmail.com>
2024-11-23 18:17:13 -0300

    com then

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
+import axios from 'axios';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [inputValue, setInputValue] = useState(null)
+    console.log(inputValue);
+
+    function fetchAddress() {
+        axios.get(`https://viacep.com.br/ws/${inputValue}/json/`)
+        .then((data) => console.log(data.data))
+        .catch((error) => console.log(error))
+        
+    }
+    
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id='container'>
+      <h1>Busca cep</h1>
+      
+          <input type="text" placeholder='Digite seu cep' onChange={(e) => setInputValue(e.target.value) } maxLength={8}/>
+          <button onClick={() => fetchAddress()}>BUSCAR</button>
+      
+    </div>
   )
 }
 
