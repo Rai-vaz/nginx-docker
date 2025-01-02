072a647a4a15685fba17ee9667dd705ba9469712
Rai-vaz <ray.vaz04@gmail.com>
2025-01-02 18:44:54 -0300

    resolvido

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# Etapa 1: Builder
+FROM node:alpine3.20 AS builder
+
+WORKDIR /app
+
+# Copiar o package.json e package-lock.json
+COPY package*.json ./
+
+# Instalar dependências
+RUN npm install
+
+# Copiar o restante da aplicação
+COPY . .
+
+# Construir a aplicação
+RUN npm run build
+
+# Etapa 2: Servir com Nginx
+FROM nginx:stable-alpine
+
+# Copiar os arquivos de build para o Nginx
+COPY --from=builder /app/dist /usr/share/nginx/html
+
+# Copiar a configuração do Nginx
+COPY ./config/nginx/nginx.conf /etc/nginx/nginx.conf
+
+# Expor a porta 80
+EXPOSE 80
+
+# Rodar o Nginx em modo foreground
+CMD ["nginx", "-g", "daemon off;"]
--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,89 @@
 import axios from 'axios';
+import {BallTriangle} from 'react-loader-spinner'
 import { useState } from 'react'
 import './App.css'
 
 function App() {
     const [inputValue, setInputValue] = useState(null)
-    console.log(inputValue);
+    const [loading, setLoading] = useState(false)
+    const [address, setAddress] = useState([])
+    console.log(address);
+    
 
     function fetchAddress() {
+        setLoading(true)
         axios.get(`https://viacep.com.br/ws/${inputValue}/json/`)
-        .then((data) => console.log(data.data))
+        .then((data) => setAddress((prevState) => [...prevState, data.data]))
         .catch((error) => console.log(error))
         
+
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000)
+
+
     }
     
 
-  return (
-    <div id='container'>
-      <h1>Busca cep</h1>
-      
-          <input type="text" placeholder='Digite seu cep' onChange={(e) => setInputValue(e.target.value) } maxLength={8}/>
-          <button onClick={() => fetchAddress()}>BUSCAR</button>
-      
-    </div>
-  )
+    return (
+        <>
+            <div id='loading'>
+                <BallTriangle
+                    height={100}
+                    width={100}
+                    radius={5}
+                    color="#4fa94d"
+                    ariaLabel="ball-triangle-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={loading}
+                
+                />
+            </div>
+
+            <div id='container'>
+            
+                <h1>Busca cep</h1>
+            
+                <input type="text" placeholder='Digite seu cep' onChange={(e) => setInputValue(e.target.value) } maxLength={8}/>
+                <button onClick={() => fetchAddress()}>BUSCAR</button>
+            
+                <table>
+                    <thead>
+                        <tr>
+                            <th>CEP</th>
+                            <th>BAIRRO</th>
+                            <th>DDD</th>
+                            <th>ESTADO</th>
+                            <th>LOCALIDADE</th>
+                            <th>LOGRADOURO</th>
+                            <th>REGIÃO</th>
+                            <th>UF</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {
+                            address.map((address) => (
+                                <tr>
+                                    <td>{address.cep}</td>
+                                    <td>{address.bairro}</td>
+                                    <td>{address.complemento}</td>
+                                    <td>{address.ddd}</td>
+                                    <td>{address.estado}</td>
+                                    <td>{address.localidade}</td>
+                                    <td>{address.logradouro}</td>
+                                    <td>{address.regiao}</td>
+                                    <td>{address.uf}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+        </>
+    )
 }
 
 export default App
